16207b774c88fc483c2f353a758ae85f26ba9ed9
Tucker J. Mayo <mistermayo@users.noreply.github.com>
2026-02-07 16:32:48 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,37 @@
+use crossterm::{
+    execute,
+    terminal::{Clear, ClearType},
+};
+use std::{io::stdout, thread, time::Duration};
+
+fn main() {
+    let mut stdout = stdout();
+    execute!(stdout, Clear(ClearType::All)).unwrap();
+
+    println!("Happy Birthday, George.");
+    println!("You awesome cat dad.\n");
+
+    let cats = [
+        "=^.^=",
+        "(=^･ω･^=)",
+        "(=ↀωↀ=)",
+        "(=｀ω´=)",
+    ];
+
+    let width = 40;
+
+    for step in 0..width {
+        execute!(stdout, Clear(ClearType::All)).unwrap();
+
+        println!("Happy Birthday, George.");
+        println!("You awesome cat dad.\n");
+
+        for (i, cat) in cats.iter().enumerate() {
+            let indent = (step + i * 6) % width;
+            println!("{:indent$}{}", "", cat, indent = indent);
+        }
+
+        thread::sleep(Duration::from_millis(120));
+    }
+}
 
